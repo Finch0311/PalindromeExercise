24c452f0d3cfc3898d6b37ea1b1ec063dfd769f1
Finch0311 <132722383+Finch0311@users.noreply.github.com>
2023-10-30 20:28:00 -0500

    Test for palindrome

---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmithTest.cs
@@ -0,0 +1,35 @@
+﻿using PalindromeExercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PalindromeExerciseTests
+{
+    public class WordSmithTest
+    {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar",true)]
+        [InlineData("Colby", false)]
+        [InlineData("coding",false)]
+        [InlineData("",false)]
+        [InlineData(null,false)]
+        [InlineData("mom", true)]
+        [InlineData("civic",true)]
+
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            //Arrange
+            var ws = new WordSmith();
+            //Act
+            var actual = ws.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+    }
+}
